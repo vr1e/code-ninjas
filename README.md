b68c8d3ee044e4091657ec3caa12221a8838f2eb
Nikola Vrhovac <vrllee@gmail.com>
2021-01-03 21:21:44 +0100

    using-flexbox

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # code-ninjas
 
+## Tailwind CSS
+
 Practice projects from [Shaun](https://github.com/iamshaunjp)'s awesome developer tutorials.
+
+[Tailwind CSS](https://www.youtube.com/playlist?list=PL4cUxeGkcC9gpXORlEHjc5bgnIi5HEGhw)
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	future: {
+		// removeDeprecatedGapUtilities: true,
+		// purgeLayersByDefault: true,
+		// defaultLineHeights: true,
+		// standardFontWeights: true
+	},
+	purge: [],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#FF6363',
+				secondary: {
+					100: '#e2e2d5',
+					200: '#888883',
+				},
+			},
+			fontFamily: {
+				body: ['Nunito'],
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
