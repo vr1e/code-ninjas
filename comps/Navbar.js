7ca3896267de8dd323499f7b58033ded859e62f7
Nikola Vrhovac <vrllee@gmail.com>
2021-08-02 12:58:51 +0200

    add images and metadata

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,14 +1,21 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
 	return (
 		<nav>
 			<div className='logo'>
-				<h1>Ninja List</h1>
+				<Image src='/logo.png' width={128} height={77} />
 			</div>
-			<Link href='/'><a>Home</a></Link>
-			<Link href='/about'><a>About</a></Link>
-			<Link href='/ninjas'><a>Ninja Listing</a></Link>
+			<Link href='/'>
+				<a>Home</a>
+			</Link>
+			<Link href='/about'>
+				<a>About</a>
+			</Link>
+			<Link href='/ninjas'>
+				<a>Ninja Listing</a>
+			</Link>
 		</nav>
 	);
 };
