b012ef624583bb799ef3d0f7455f3b24380a5010
Nikola Vrhovac <vrllee@gmail.com>
2021-08-02 12:30:16 +0200

    add styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import Layout from '../comps/Layout'
-import '../styles/globals.css'
+import Layout from '../comps/Layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
+	return (
 		<Layout>
 			<Component {...pageProps} />
 		</Layout>
 	);
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
 import Link from 'next/link';
-import Footer from '../comps/Footer';
-import Navbar from '../comps/Navbar';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
 	return (
 		<div>
-			<h1>Homepage</h1>
-			<p>
+			<h1 className={styles.title}>Homepage</h1>
+			<p className={styles.text}>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius itaque cum
 				error quia a maxime amet consequuntur ipsa illum numquam veniam fugiat
 				saepe, ratione quis sed, quae quas laudantium ea?
 			</p>
-			<p>
+			<p className={styles.text}>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius itaque cum
 				error quia a maxime amet consequuntur ipsa illum numquam veniam fugiat
 				saepe, ratione quis sed, quae quas laudantium ea?
 			</p>
 			<Link href='/ninjas'>
-				<a>See Ninja Listing</a>
+				<a className={styles.btn}>See Ninja Listing</a>
 			</Link>
 		</div>
 	);
