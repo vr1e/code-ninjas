fd33e0192ccac83758e8be2b4c1bce30f54cd530
Nikola Vrhovac <vrllee@gmail.com>
2021-08-05 18:41:49 +0200

    add rich text from contentful

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,4 +1,6 @@
 import { createClient } from 'contentful';
+import Image from 'next/image';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const client = createClient({
 	space: process.env.CONTENTFUL_SPACE_ID,
@@ -34,6 +36,56 @@ export async function getStaticProps({ params }) {
 }
 
 export default function RecipeDetails({ recipe }) {
-	console.log(recipe);
-	return <div>Recipe Details</div>;
+	const { featuredImage, title, cookingTime, ingredients, method } =
+		recipe.fields;
+	return (
+		<div>
+			<div className='banner'>
+				<Image
+					src={'https:' + featuredImage.fields.file.url}
+					width={featuredImage.fields.file.details.image.width}
+					height={featuredImage.fields.file.details.image.height}
+				/>
+				<h2>{title}</h2>
+				<div className='info'>
+					<p>Take about {cookingTime} mins to cook.</p>
+					<h3>Ingredients:</h3>
+
+					{ingredients.map(ing => (
+						<span key={ing}>{ing}</span>
+					))}
+				</div>
+				<div className='method'>
+					<h3>Method:</h3>
+					<div>{documentToReactComponents(method)}</div>
+				</div>
+			</div>
+			<style jsx>{`
+				h2,
+				h3 {
+					text-transform: uppercase;
+				}
+				.banner h2 {
+					margin: 0;
+					background: #fff;
+					display: inline-block;
+					padding: 20px;
+					position: relative;
+					top: -60px;
+					left: -10px;
+					transform: rotateZ(-1deg);
+					box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+				}
+				.info p {
+					margin: 0;
+				}
+				.info span::after {
+					content: ', ';
+				}
+				.info span:last-child::after {
+					content: '.';
+				}
+			`}</style>
+		</div>
+	);
 }
