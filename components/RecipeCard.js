244e4d30f6b2cf43786521f43256e592be07978f
Nikola Vrhovac <vrllee@gmail.com>
2021-08-05 14:25:31 +0200

    add contentful and images

---
+++ components/RecipeCard.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+const RecipeCard = ({ recipe }) => {
+	const { title, slug, cookingTime, thumbnail } = recipe.fields;
+
+	return (
+		<div className='card'>
+			<div className='featured'>
+				<Image
+					src={'https:' + thumbnail.fields.file.url}
+					width={thumbnail.fields.file.details.image.width}
+					height={thumbnail.fields.file.details.image.height}
+				/>
+			</div>
+			<div className='content'>
+				<div className='info'>
+					<h4>{title}</h4>
+					<p>Takes approx {cookingTime} mins to make</p>
+					<div className='actions'>
+						<Link href={'/recipes/' + slug}>
+							<a>Cook this</a>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default RecipeCard;
