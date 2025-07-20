2c83add0fb1d5d68cca3d41d20b9119dd6ec175f
Nikola Vrhovac <vrllee@gmail.com>
2025-07-20 18:49:55 +0200

    flutter basics

---
+++ coffee_card/lib/home.dart
@@ -0,0 +1,43 @@
+import 'package:coffee_card/coffee_prefs.dart';
+import 'package:coffee_card/styled_body_text.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My Coffee Id',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.brown[700],
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.brown[200],
+            padding: const EdgeInsets.all(20),
+            child: StyledBodyText("How I like my coffee..."),
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: const EdgeInsets.all(20),
+            child: const CoffeePrefs(),
+          ),
+          Expanded(
+            child: Image.asset(
+              'assets/img/coffee_bg.webp',
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.bottomCenter,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ coffee_card/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:coffee_card/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(height: 100, color: Colors.red, child: const Text("one")),
+          Container(height: 100, color: Colors.green, child: const Text("two")),
+          Container(
+            height: 100,
+            color: Colors.blue,
+            child: const Text("three"),
+          ),
+        ],
+      ),
+    );
+  }
+}
